98b3415e006b6c5e46074637dc742705a307dcfe
mjhallman <matt.hallman@jorsek.com>
2023-03-13 15:01:12 -0400

    fix(elements-core): changes to allow showing single operation viewer

--- examples/react-cra/src/components/API.tsx
+++ examples/react-cra/src/components/API.tsx
@@ -1,13 +1,87 @@
-import '@stoplight/elements/styles.min.css';
-
-import { API } from '@stoplight/elements';
+import { API, transformOasToServiceNode } from '@stoplight/elements';
+// @ts-ignore
+import {
+  InlineRefResolverProvider,
+  ParsedDocs,
+  useBundleRefsIntoDocument,
+  useParsedValue,
+} from '@stoplight/elements-core';
+import { NodeType } from '@stoplight/types';
 import React from 'react';
+import { useQuery } from 'react-query';
+import { useLocation } from 'react-router-dom';
+
+//   <ParsedDocs
+//     key={pathname}
+//     uri={pathname}
+//     node={node}
+//     nodeTitle={node.name}
+//     layoutOptions={layoutOptions}
+//     location={location}
+//   />
 
 export const StoplightAPI: React.FC = () => {
+  const apiDescriptionDocument = undefined;
+  // const apiDescriptionUrl =
+  //   'https://raw.githubusercontent.com/stoplightio/Public-APIs/master/reference/zoom/openapi.yaml';
+  // const apiDescriptionUrl = 'https://petstore.swagger.io/v2/swagger.json';
+  const apiDescriptionUrl = 'http://localhost:8000/reltio-openApi.sanistized.json';
+
+  const { data: fetchedDocument, error } = useQuery(
+    [apiDescriptionUrl],
+    () =>
+      fetch(apiDescriptionUrl).then(res => {
+        if (res.ok) {
+          return res.text();
+        }
+        throw new Error(`Unable to load description document, status code: ${res.status}`);
+      }),
+    {
+      // enabled: apiDescriptionUrl !== '' && !apiDescriptionDocument,
+    },
+  );
+  // const location = useLocation();
+  const document = apiDescriptionDocument || fetchedDocument || '';
+  const parsedDocument = useParsedValue(document);
+  const bundledDocument = useBundleRefsIntoDocument(parsedDocument, { baseUrl: apiDescriptionUrl });
+  const serviceNode = React.useMemo(() => {
+    return transformOasToServiceNode(bundledDocument);
+  }, [bundledDocument]);
+
+  const pathname = '/operations/accounts';
+  const node = serviceNode ? serviceNode.children.find(child => child.uri === pathname) : undefined;
+
+  const hideTryIt = false;
+  const hideExport = true;
+
+  const layoutOptions = React.useMemo(
+    () => ({ hideTryIt: hideTryIt, hideExport: hideExport }),
+    [hideTryIt, hideExport],
+  );
+
+  const pd =
+    node === undefined ? (
+      <div>Loading...</div>
+    ) : (
+      <InlineRefResolverProvider document={parsedDocument}>
+        <ParsedDocs
+          key={pathname}
+          uri={pathname}
+          // @ts-ignore
+          node={node}
+          nodeTitle={node?.name}
+          layoutOptions={layoutOptions}
+          // location={location}
+        />
+      </InlineRefResolverProvider>
+    );
+
+  // return pd;
   return (
     <API
       basePath="zoom-api"
-      apiDescriptionUrl="https://raw.githubusercontent.com/stoplightio/Public-APIs/master/reference/zoom/openapi.yaml"
+      // apiDescriptionUrl="https://raw.githubusercontent.com/stoplightio/Public-APIs/master/reference/zoom/openapi.yaml"
+      apiDescriptionUrl="http://localhost:8000/reltio-openApi.sanistized.json"
     />
   );
 };
---
+++ mattnotes.md
@@ -0,0 +1,13 @@
+
+## Building
+1. `nvm use`
+2. Change version in packages/elements/package.json
+3. At the root, run `yarn build`
+           
+
+## Publishing
+1. `cd packages/elements/dist`
+2. `npm publish --access=public`
+
+## Other notes
+* Project is built with lerna. It might be better to fork the whole repo, and use lerna.
